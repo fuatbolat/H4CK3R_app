f1ee393dfb9ce6c3dfcb1abf497c5b43ea23a6f2
fuatbolat <byfuatbolat@gmail.com>
2024-02-03 12:24:44 +0300

    Initial Commit

---
+++ H4CK3Rapp/H4CK3RappApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4CK3RappApp.swift
+//  H4CK3Rapp
+//
+//  Created by Fuat Bolat on 3.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct H4CK3RappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
